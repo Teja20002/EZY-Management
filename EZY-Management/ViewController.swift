251d428f25731b7a2cc707dc4692798c5bce2fa3
Teja20002 <tejamanchala91@gmail.com>
2024-11-09 07:50:53 -0600

    Added login screen UI elements and basic IBAction setup

--- EZY-Management/ViewController.swift
+++ EZY-Management/ViewController.swift
@@ -12,8 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
-
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        // Check if both text fields are not empty
+            guard let username = usernameTextField.text, !username.isEmpty,
+                  let password = passwordTextField.text, !password.isEmpty else {
+                print("Username or password field is empty")
+                return
+            }
+            
+            // Proceed with login (we'll add more functionality here later)
+            print("Logging in with username: \(username) and password: \(password)")
+    }
+    
+    
 }
 
