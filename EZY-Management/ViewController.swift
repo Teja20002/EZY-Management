822183238ff64f4589ed9af9949c68d7293ae94e
Teja20002 <tejamanchala91@gmail.com>
2024-11-09 06:42:05 -0600

    Initial commit with Xcode project setup

---
+++ EZY-Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EZY-Management
+//
+//  Created by Teja Manchala on 11/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
